f80791fce76628704a6d588e83b198cf44107bcd
Sang <75106540+sang1833@users.noreply.github.com>
2023-12-02 09:53:58 +0700

    main

--- src/apis/main_api.ts
+++ src/apis/main_api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const baseURL = "https://be-se100.onrender.com";
-export const baseURL = "http://localhost:9000";
+export const baseURL = "https://be-se100.onrender.com";
+// export const baseURL = "http://localhost:9000";
 
 export const mainApi = axios.create({
   baseURL: baseURL,
