4bafb0d98649829d4f18f03e3a39e19646b2be01
Sang <75106540+sang1833@users.noreply.github.com>
2023-12-27 14:18:30 +0700

    new api

--- src/apis/main_api.ts
+++ src/apis/main_api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "https://se100.onrender.com/api";
+export const baseURL = "https://se100.azurewebsites.net/api";
 // export const baseURL = "http://localhost:5000/api";
 export const hostURL = "http://localhost:5000";
 
